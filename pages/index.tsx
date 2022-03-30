0572eefb0cca4ca5a8e205526ad0d244ab67602b
eunjinlee <lucy7xoxo@naver.com>
2022-03-30 22:26:58 +0900

    [Add] Header UI 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-      </div>
-      <h1>Lets build airbnb</h1>
-    </>
+    <div className="">
+      <Head>
+        <title>Stay at Eunjin's Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      {/* Banner */}
+    </div>
   )
 }
 
